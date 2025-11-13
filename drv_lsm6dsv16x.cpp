ac1cc774ef950779e548a04314f222b818ca646c
Chimipupu <chiminyanyan@gmail.com>
2025-11-13 23:40:21 +0900

    LSM6DSV16Xのレジスタアドレスを定義

---
+++ drv_lsm6dsv16x.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file drv_lsm6dsv16x.cpp
+ * @author Chimipupu(https://github.com/Chimipupu)
+ * @brief 6軸IMU LSM6DSV16Xドライバ
+ * @version 0.1
+ * @date 2025-11-13
+ * 
+ * @copyright Copyright (c) 2025 Chimipupu All Rights Reserved.
+ * 
+ */
+
+#include "drv_lsm6dsv16x.h"
+
+void drv_lsm6dsv16x_init(void)
+{
+    // TODO: 実装
+}
